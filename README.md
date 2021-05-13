e7a93f6a9a2bd073ce0f0f7485ac20cfbe7828d3
Luis Filipe Guedes Mota <lmota@img.com.br>
2021-05-13 19:30:50 -0300

    Por que dubê de testes

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 **Curso da Alura de Testes em Python Trabalhando com duble de testes - 2021** - [https://cursos.alura.com.br/course/python-testes-com-dubles](https://cursos.alura.com.br/course/python-testes-com-dubles)
 
 **Conceitos Abordados:**
+- que precisamos de dublês para simular acesso à internet, ao sistema de arquivos e banco de dados
+- que os dublês servem para substituir partes do sistema que não estão implementadas
+- que os dublês servem para simular comportamentos para que os testes tenham resultados determinísticos
---
+++ tests/test_livros.py
@@ -0,0 +1,32 @@
+from unittest import skip
+from unittest.mock import patch
+
+import pytest
+
+from colecao.livros import consultar_livros
+
+@skip("Vale quando consultar livros estiver completo")
+def test_consultar_livros_retorna_resultado_formato_string():
+    resultado = consultar_livros("Agatha Christie")
+    assert type(resultado) == str
+
+
+def test_consultar_livros_chama_preparar_dados_para_requisicao_uma_vez_e_com_os_mesmos_parametros_de_consultar_livros():
+    with patch("colecao.livros.preparar_dados_para_requisicao") as duble:
+        consultar_livros("Agatha Christie")
+        duble.assert_called_once_with("Agatha Christie")
+
+def test_consultar_livros_chama_obter_url_usando_como_parametro_o_retorno_de_preparar_dados_para_requisicao():
+    with patch("colecao.livros.preparar_dados_para_requisicao") as duble_preparar_dados_para_requisicao:
+        dados = {"author": "Agatha Christie"}
+        duble_preparar_dados_para_requisicao.return_value = dados
+        with patch("colecao.livros.obter_url") as duble_obter_url:
+            consultar_livros("Agatha Christie")
+            duble_obter_url.assert_called_once_with("http://buscador", dados)
+
+def test_consultar_livros_chama_executar_requisicao_usando_retorno_obter_url():
+    with patch("colecao.livros.obter_url") as duble_obter_url:
+        duble_obter_url.return_value = "http://buscador"
+        with patch("colecao.livros.executar_requisicao") as duble_executar_requisicao:
+            consultar_livros("Agatha Christie")
+            duble_executar_requisicao.assert_called_once_with("http://buscador")
